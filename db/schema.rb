6e4dbe0ea5af2aea4c667e4c7806d6c00087dcff
emily doswell <emilydoswell@yahoo.co.uk>
2021-05-26 13:51:07 +0200

    Change content column from string to text in reviews and change restaurant id to add foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_111048) do
+ActiveRecord::Schema.define(version: 2021_05_26_113345) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(version: 2021_05_26_111048) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.string "content"
-    t.integer "restaurant_id"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
