f8acd0a5b0749cee221b3aa71a189327626bb1a8
emily doswell <emilydoswell@yahoo.co.uk>
2021-05-26 16:34:39 +0200

    created seed and added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  # Validations
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times { Restaurant.create{name: 'Pizza Hut', phone_number: '08838202', address: 'The street, Street, NR29 4DF', category: 'chinese'} }
